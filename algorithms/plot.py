0f344eb6d458e5563b1d50a2af6456acbf5e2a56
David Mears <kieukmail@gmail.com>
2019-06-05 13:24:58 +0100

    plotter saves files

--- algorithms/plot.py
+++ algorithms/plot.py
@@ -1,2 +1,16 @@
+import os
+from datetime import datetime
+
+import matplotlib
+import matplotlib.pyplot as plt
+
 class Plotter:
-    pass
+    
+    def plot(self, x_label, x_data, y_label, y_data):
+        plt.plot([3, 4], [3, 4])
+        plt.xlabel('number of eggs eaten')
+        plt.ylabel('stars in the sky')
+        file_name = ('%s.png' % str(datetime.now().timestamp()))
+        file_path = os.path.join(os.getcwd(), 'plots', file_name)
+        plt.savefig(file_path)
+        return file_path
