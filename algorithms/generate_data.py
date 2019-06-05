15172062962814e2c1c8de7f5abea806838977f3
David Mears <kieukmail@gmail.com>
2019-06-05 14:15:10 +0100

    DataGenerator can interpolate n values

--- algorithms/generate_data.py
+++ algorithms/generate_data.py
@@ -1,4 +1,11 @@
 from algorithms import timer
 
 class DataGenerator():
-    pass
+    
+    def get_n_values(self, increment=500, maximum=10000):
+        n_values = []
+        while increment <= maximum:
+            n_values.append(maximum)
+            maximum -= increment
+        n_values.reverse()
+        return n_values
--- test_generate_data.py
+++ test_generate_data.py
@@ -3,9 +3,17 @@ from algorithms import generate_data
 
 class DataGeneratorTest(unittest.TestCase):
 
-    def test_can_make_data_generator(self):
+    def setUp(self):
         self.data_generator = generate_data.DataGenerator()
-        self.assertTrue(self.data_generator)
+
+    def test_can_calculate_values_of_n(self):
+        n_values = self.data_generator.get_n_values(
+            increment=100,
+            maximum=1000,
+        )
+        self.assertEqual(n_values, [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
+
+    
 
 if __name__ == '__main__':
     unittest.main()
