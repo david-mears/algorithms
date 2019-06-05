1f661cbe5f3d3a0781c5c1dcbd282a9412aba441
David Mears <kieukmail@gmail.com>
2019-06-05 14:42:10 +0100

    save point of TDD

--- algorithms/generate_data.py
+++ algorithms/generate_data.py
@@ -1,8 +1,18 @@
 from algorithms import timer
 
 class DataGenerator():
-    
-    def get_n_values(self, increment=500, maximum=10000):
+    INCREMENT = 500
+    MAXIMUM = 10000
+
+    def time_with_various_n(
+        self,
+        function,
+        increment=INCREMENT,
+        maximum=MAXIMUM,
+        ):
+        pass
+
+    def __get_n_values(self, increment, maximum):
         n_values = []
         while increment <= maximum:
             n_values.append(maximum)
